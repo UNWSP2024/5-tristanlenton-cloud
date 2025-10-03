bf165a7c52225be650ad43cbd9d3df220d84808c
tristanlenton-cloud <tristan.lenton@gmail.com>
2025-10-02 19:45:40 -0700

    Add files via upload

--- program_2.py
+++ program_2.py
@@ -1,13 +1,14 @@
-# Program #2: Math Quiz
-# Write a program that gives simple math quizzes.  The program should display two random numbers to be added, such as
+import random
 
-#     247
-
-# + 129
-
-# ------
-
-# The program should allow the student to enter the answer.  
-# If the answer is correct, a message of congratulations should be displayed.  
-# If the answer is incorrect a message showing the correct answer should be displayed.  
-# The program must use a function that accomplishes part of the needed tasks.
+def mathProblems():
+    num1 = random.randint(100,999)
+    num2 = random.randint(100,999)
+    print(" ",num1)
+    print("+",num2)
+    print("-----")
+    answer = int(input("Enter your answer: "))
+    if answer == (num1+num2):
+        print("Congratulations!")
+    else:
+        print("The correct answer was", num1+num2)
+mathProblems()
